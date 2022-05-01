85a122e2c38a8c1197937e36893b2681a401b35d
Defconn4 <fcos624@gmail.com>
2022-05-01 18:22:20 -0400

    added special song tokens for lyrics && skeleton of model training

--- fetch_lyrics.py
+++ fetch_lyrics.py
@@ -3,6 +3,8 @@ import os
 from os.path import exists
 from pathlib import Path
 
+from regex import E
+
 # INSERT API client ID & tokens here.
 # TODO: REMOVE THESE BEFORE MAKING PUBLIC COMMITS.
 client_id = 'YsrwCCloXJysedZMDINWA-LItizvvaWZ2IGtxMlHYQ98R9G3sorFRo_rRLQGirRq'
@@ -33,34 +35,49 @@ class Lyric_Grabber:
             per_page=20, get_full_info=True, 
             allow_name_change=True, artist_id=None,
             include_features=False)
+
+        NOTE: Special/Accented characters will cause genius.search_artist to return NoneType (e.g. André 3000)
+              Luckily, genius.search_artist will automatically replace regular characters with
+              accented counterparts before querying.
     """
     def create_song_list(self):
-        artist = genius.search_artist(self.artist_name, max_songs=20,
-                                    sort='popularity', get_full_info=False)         # Artist's names that are not exact as they appear on Genius will be modifed upon query.
 
-        """
-        # Save artist lyrics to [ARTIST_NAME].txt
-        artist.save_lyrics(filename=self.artist_name, extension='txt',
-                            overwrite=False, ensure_ascii=True,
-                            sanitize=True, verbose=False)
-        """ 
+        try:
+            artist = genius.search_artist(self.artist_name, max_songs=20,
+                                        sort='popularity', get_full_info=False)         # Artist's names that are not exact as they appear on Genius will be modifed upon query.
 
-        # If artist lyric file doesn't already exist, open for writing (i.e. create file if it doesn't already exist).
-        cwd = str(Path.cwd()) + '\\lyrics'
-        file_name = os.path.join(cwd, self.artist_name)
-        
-        # If file already exists, then we can exit the function.
-        if os.path.exists('.\lyrics\\' + file_name + '.txt'):
-            print("<%s.txt> already exists in .\lyrics\\ directory, exiting function..." % self.artist_name)
-            return
-        else:
-            lyric_file = open(file_name + '.txt', 'w', encoding='utf-8')
+            """
+            # Save artist lyrics to [ARTIST_NAME].txt
+            artist.save_lyrics(filename=self.artist_name, extension='txt',
+                                overwrite=False, ensure_ascii=True,
+                                sanitize=True, verbose=False)
+            """ 
 
-            # NOTE: Alternatively, you can use genius.save_artists(artists, filename='artist_lyrics', overwrite=False, ensure_ascii=True)
-            #       to save lyrics of multiple artists all at once into a single file - saves as JSON. 
-            # Write lyrics to file and close when done.
-            for song in artist.songs:
-                lyric_file.write(song.lyrics)
-                lyric_file.write("\n\n")
-                
-            lyric_file.close()
+            # If artist lyric file doesn't already exist, open for writing (i.e. create file if it doesn't already exist).
+            cwd = str(Path.cwd()) + '\\lyrics'
+            file_name = os.path.join(cwd, self.artist_name)
+            
+            # If file already exists, then we can exit the function.
+            if os.path.exists('.\lyrics\\' + file_name + '.txt'):
+               print("<%s.txt> already exists in .\lyrics\\ directory, exiting function..." % self.artist_name)
+               return
+            else:
+                lyric_file = open(file_name + '.txt', 'w', encoding='utf-8')
+
+                # NOTE: Alternatively, you can use genius.save_artists(artists, filename='artist_lyrics', overwrite=False, ensure_ascii=True)
+                #       to save lyrics of multiple artists all at once into a single file - saves as JSON. 
+                # Write lyrics to file and close when done.
+                for song in artist.songs:
+                    lyric_file.write("<|BeginSong|>")       # Special token to denote beginning of a song.
+                    lyric_file.write("\n")
+                    lyric_file.write(song.lyrics)
+                    lyric_file.write("\n")
+                    lyric_file.write("<|EndSong|>")         # Special token to denote the end of a song.
+                    lyric_file.write("\n\n")
+
+                lyric_file.close()
+
+        except Exception as e:
+            err_message = "Artist with name <{}> caused the following error:\n\t{}".format(self.artist_name, e)
+            print(err_message)
+            pass
--- rap_model.py
+++ rap_model.py
@@ -1,9 +1,8 @@
-from transformers import GPT2Tokenizer, TFGPT2Model         # Need to use Python 3.8.12 (anaconda `cs499` environment) as interpreter
+from unittest.util import _MAX_LENGTH
+import tokenizers
+from transformers import GPT2Tokenizer, GPT2Model, pipeline, set_seed, TFGPT2Model, GPT2LMHeadModel         # Need to use Python 3.8.12 (anaconda `cs499` environment) as interpreter
+import torch
 from fetch_lyrics import *
-
-## Single object test:
-# young_sinatra = Lyric_Grabber('Logic')
-# young_sinatra.create_song_list()
 
 """
     Iterate through list_rappers.txt & make list of artists.
@@ -28,13 +27,88 @@ def create_lyrics(artists_list):
         print("Lyrics for <%s> written to .\lyrics\ directory!\n" % name)
     print("\n\nDone writing lyrics to directory!\n\n")
 
-# Generate lyrics for rappers found in `list_rappers.txt`
-artists_list = list_all_rappers('.\data\list_rappers.txt', 76)
-create_lyrics(artists_list)
+"""
+    Probe user for artist lyrics to generate.
+"""
+def user_select_artist():
+    user_artist = None
 
-# Ask the user what artist they would like to generate lyrics for - this will later be used in Lyric_Grabber objects.
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model = TFGPT2Model.from_pretrained('gpt2')
-text = "The Industrial Revolution has destroyed the foundations of American culture."
-encoded_input = tokenizer(text, return_tensors='tf')
-output = model(encoded_input)
+    # Search .\lyrics\ for the file containing that artist's name, if it doesn't exist, prompt the user again.
+    while user_artist == None:
+        user_artist = input("Please type the full name (proper spelling and capitalization) of the rapper you wish to generate lyrics in the spirit of: ")
+        if not os.path.exists('.\lyrics\\' + user_artist + '.txt'):
+            print("CoZZy cannot find a rapper with name <%s> in his memory, please type a different rapper..." % user_artist, "\n\n")
+            user_artist = None
+        else:
+            print("CoZZy will learn to rap like : <%s>" % user_artist)
+            artist_lyric_file_path = '.\lyrics\\' + user_artist + '.txt'
+            break
+    return user_artist, artist_lyric_file_path
+
+
+"""
+    Initialize model and tokenizer.
+"""
+def initialize_model():
+    print("Initializing model...\n")
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+    return model, tokenizer
+
+
+"""
+    Create a tensor of the for the given artist selected by the user.
+    I.e. Tokenize the input text - lyrics of 20 songs by artists
+    This tensor will be used in model training.
+"""
+def create_lyric_tensor(artist_lyric_file_path, tokenizer):
+    lyrics = []                                             # List of torch tensors containing encoded lyrics.
+    with open(artist_lyric_file_path, 'r') as f:
+        for line in f:
+            lyrics.append(torch.tensor(tokenizer.encode()))
+    return None
+    #return torch.tensor(None)
+
+"""
+    Train GPT model on given artist.
+    Model evaulation will be done in here too.
+
+    TODO: Alternatively, create a vocabulary here of all songs from the .\lyrics\ and then use that to aid in training.
+    NOTE: GPT2Tokenizer takes a vocabulary input: https://huggingface.co/docs/transformers/model_doc/gpt2 
+"""
+def train():
+
+    # NOTE: Generate lyrics for rappers found in `list_rappers.txt`
+    #       need to be only done once UNLESS new artists are added to .\data\list_rappers.txt
+    #       AND/OR changes are made to code in `fetch_lyrics.py`.
+    artists_list = list_all_rappers('.\data\list_rappers.txt', 76)
+    create_lyrics(artists_list)
+    
+    user_artist, artist_lyric_file_path = user_select_artist()
+    model, tokenizer = initialize_model()
+    #lyric_tensor = create_lyric_tensor(artist_lyric_file_path, tokenizer)
+
+    print("CoZZy is writing his lyrics...\n")
+
+    # encoded_input = tokenizer(artist_lyric_file_path, return_tensors='pt')
+    # --------------------------------------------------------------------
+    # text = "Hello my name is Jeff."
+
+    # encoded_input = tokenizer(text, return_tensors='pt')
+    # outputs = model(**encoded_input, labels=encoded_input["input_ids"])
+    # loss = outputs.loss
+    # logits = outputs.logits
+
+    # print(loss)
+    # print(logits)
+    # --------------------------------------------------------------------
+    #generator = pipeline('text-generation', model = 'gpt2')
+    #set_seed(42)
+    #print(generator(artist_lyric_file_path, max_length=100, num_return_sequences=5))
+
+    #output_tokens = model.generate(encoded_input, max_length = 200, do_sample = True)
+    #lyrics = tokenizer.decode(output_tokens)
+    #print(lyrics)
+
+train()
